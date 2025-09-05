f932f4d72678894172501e2646187b2b1054deea
Dan Dressler <dan.dressler@slalom.com>
2025-09-04 18:20:33 -0700

    Add project files.

---
+++ WotrModDragonSubdomain/Main.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+using Kingmaker.Blueprints.JsonSystem;
+using UnityModManagerNet;
+
+namespace WotrModDragonSubdomain
+{
+#if DEBUG
+    [EnableReloading]
+#endif
+    public static class Main
+    {
+        internal static Harmony HarmonyInstance;
+        internal static UnityModManager.ModEntry.ModLogger log;
+
+        public static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            log = modEntry.Logger;
+#if DEBUG
+            modEntry.OnUnload = OnUnload;
+#endif
+            modEntry.OnGUI = OnGUI;
+            HarmonyInstance = new Harmony(modEntry.Info.Id);
+            HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+            return true;
+        }
+
+        public static void OnGUI(UnityModManager.ModEntry modEntry)
+        {
+
+        }
+
+#if DEBUG
+        public static bool OnUnload(UnityModManager.ModEntry modEntry) {
+            HarmonyInstance.UnpatchAll(modEntry.Info.Id);
+            return true;
+        }
+#endif
+        [HarmonyPatch(typeof(BlueprintsCache))]
+        public static class BlueprintsCaches_Patch
+        {
+            private static bool Initialized = false;
+
+            [HarmonyPriority(Priority.First)]
+            [HarmonyPatch(nameof(BlueprintsCache.Init)), HarmonyPostfix]
+            public static void Init_Postfix()
+            {
+                try
+                {
+                    if (Initialized)
+                    {
+                        log.Log("Already initialized blueprints cache.");
+                        return;
+                    }
+                    Initialized = true;
+
+                    log.Log("Patching blueprints.");
+                    // Insert your mod's patching methods here
+                    DragonSubdomain.Configure();
+                    // Example
+                    // SuperAwesomeFeat.Configure()
+                }
+                catch (Exception e)
+                {
+                    log.Log(string.Concat("Failed to initialize.", e));
+                }
+            }
+        }
+    }
+}
