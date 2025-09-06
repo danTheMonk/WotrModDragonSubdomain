3da8328ed2942dc3f067a97aad610a2885531e53
Dan Dressler <dan.dressler@slalom.com>
2025-09-05 23:55:28 -0700

    Moved repeated reference into constants.

---
+++ WotrModDragonSubdomain/Constants.cs
@@ -0,0 +1,11 @@
+﻿using BlueprintCore.Blueprints.References;
+using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.Classes;
+
+namespace WotrModDragonSubdomain
+{
+    public static class Constants
+    {
+        public static readonly BlueprintReference<BlueprintFeature> dragonBreathFeatureReference = FeatureRefs.BloodlineDraconicBlackBreathWeaponFeature.Reference; // Still need to add actual breath feature or create one if needed
+    }
+}
